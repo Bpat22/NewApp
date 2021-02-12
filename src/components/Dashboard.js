fbb684398b39a762a99b7536fb5b2fa1e12c7d24
bthomp22 <brentthompson2012@gmail.com>
2021-02-12 13:51:10 -0500

    getting data on dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom';
+import { User } from '../redux/user';
 
-export default function Dashboard(props) {
-    return (
+
+const mapStateToProps = state => {
+    return {
+        user: state.user,
+        token: state.token
+    }
+}
+class Dashboard extends Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        const test = this.props.user;
+        console.log(this.props);
+         return (
         <div>
             <h3>Dashboard</h3>
+        <p>{this.props.user.firstName}</p>
         </div>
     )
+    }
+    
+   
 }
+
+export default withRouter(connect(mapStateToProps)(Dashboard));
+//export default Dashboard;
